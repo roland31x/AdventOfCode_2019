3cfae8ef912f301f2c676e15a56f417ad6951f0d
roland31x <roland31x@gmail.com>
2023-07-30 00:39:13 +0300

    day 1 done

---
+++ D01/D01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace D01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"..\..\..\input.txt");
+            int sum1 = 0;
+            int sum2 = 0;
+            for(int i = 0; i < lines.Length; i++)
+            {
+                int modulefuel = int.Parse(lines[i]) / 3 - 2;
+                sum1 += modulefuel;
+                do
+                {
+                    sum2 += modulefuel;
+                    modulefuel /= 3;
+                    modulefuel -= 2;
+                } while (modulefuel > 0);
+            }
+            Console.WriteLine("Part 1 solution:");
+            Console.WriteLine(sum1);
+            Console.WriteLine("Part 2 solution:");
+            Console.WriteLine(sum2);
+        }
+    }
+}
