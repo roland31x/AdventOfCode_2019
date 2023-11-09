fa8a1484e94cfb384e246aa9c6e5d6f9672c3376
roland31x <roland31x@gmail.com>
2023-11-09 17:31:12 +0200

    day 21 done, day 22 part 1 done

---
+++ D21/D21/Program.cs
@@ -0,0 +1,264 @@
+﻿namespace D21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lines = File.ReadAllText(@"..\..\..\input.txt");
+            Part1(lines);
+            Part2(lines);
+        }
+        static void Part1(string lines)
+        {
+            IntCode prog = new IntCode(lines);
+            prog.Run();
+            List<string> myprog = JumpProgram4Steps();
+            foreach (string s in myprog)
+                prog.InputASCII(s);
+            prog.InputASCII("WALK");
+            prog.Run();
+            Console.WriteLine("Part 1 solution:");
+            Console.WriteLine(prog.outputs.Last());
+        }
+        static void Part2(string lines)
+        {
+            IntCode prog = new IntCode(lines);
+            prog.Run();
+            prog.OutputASCII();
+            List<string> myprog = JumpProgram9Steps();
+            foreach (string s in myprog)
+                prog.InputASCII(s);
+            prog.InputASCII("RUN");
+            prog.Run();
+            prog.OutputASCII();
+            Console.WriteLine("Part 2 solution:");
+            Console.WriteLine(prog.outputs.Last());
+        }
+        static List<string> JumpProgram4Steps() // A B C D
+        {
+            List<string> toreturn = new List<string>() 
+            {               
+                "NOT A J",
+                "NOT B T",
+                "OR T J",
+                "NOT C T",
+                "OR T J",
+                "AND D J", // jumps to D only if there is a hole in A B or C
+            };
+
+            return toreturn;
+        }
+        static List<string> JumpProgram9Steps() // A B C D E F G H I
+        {
+            List<string> toreturn = new List<string>()
+            {
+                "NOT A J",
+                "NOT B T",
+                "OR T J",
+                "NOT C T",
+                "OR T J",
+                "AND D J", // jumps to d only if h is also jumpable
+                "AND H J",
+                "NOT A T", // jumps every time at latest point
+                "OR T J",             
+            };
+
+            return toreturn;
+        }
+    }
+    public class IntCode
+    {
+        public long[] program;
+        long driver = 0;
+        long relativebase = 0;
+        int memsize = 10000; // please adjust this 
+        public List<long> outputs = new List<long>();
+        public Queue<long> inputs = new Queue<long>();
+        int definput = 0;
+        public IntCode(string ops)
+        {
+            string[] tokens = ops.Split(',');
+            program = new long[memsize];
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                program[i] = long.Parse(tokens[i]);
+            }
+        }
+        public void InputASCII(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+                inputs.Enqueue((int)input[i]);
+            inputs.Enqueue(10);
+        }
+        public void OutputASCII()
+        {
+            foreach (int i in outputs)
+                if(i < 256)
+                    Console.Write((char)i);
+        }
+        public long Output()
+        {
+            return outputs.Last();
+        }
+        public bool Run()
+        {
+            checked
+            {
+                while (program[driver] != 99)
+                {
+                    try
+                    {
+                        Dissect(program[driver]);
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        public long GetMode(long mode, long pos)
+        {
+            try
+            {
+                switch (mode)
+                {
+                    case 0:
+                        return program[pos];
+                    case 1:
+                        return pos;
+                    case 2:
+                        return program[pos] + relativebase;
+                }
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+            throw new Exception("unkown mode");
+        }
+        void Dissect(long opcode)
+        {
+            long[] op = new long[5];
+            int idx = 4;
+            while (opcode > 0)
+            {
+                op[idx] = opcode % 10;
+                opcode /= 10;
+                idx--;
+            }
+            long firstparam = GetMode(op[2], driver + 1);
+            long secondparam = GetMode(op[1], driver + 2);
+            long thirdparam = GetMode(op[0], driver + 3);
+
+            long actualcode = 10 * op[3] + op[4];
+            switch (actualcode)
+            {
+                case 1:
+                    Addition(firstparam, secondparam, thirdparam);
+                    driver += 4;
+                    break;
+                case 2:
+                    Multiply(firstparam, secondparam, thirdparam);
+                    driver += 4;
+                    break;
+                case 3:
+                    Input(firstparam);
+                    driver += 2;
+                    break;
+                case 4:
+                    Output(firstparam);
+                    driver += 2;
+                    break;
+                case 5:
+                    if (!JumpIfTrue(firstparam, secondparam))
+                        driver += 3;
+                    break;
+                case 6:
+                    if (!JumpIfFalse(firstparam, secondparam))
+                        driver += 3;
+                    break;
+                case 7:
+                    LessThan(firstparam, secondparam, thirdparam);
+                    driver += 4;
+                    break;
+                case 8:
+                    Equality(firstparam, secondparam, thirdparam);
+                    driver += 4;
+                    break;
+                case 9:
+                    SetRelativeBase(firstparam);
+                    driver += 2;
+                    break;
+                default:
+                    throw new Exception(actualcode + " unknown opcode!");
+
+            }
+        }
+        void SetRelativeBase(long firstparam)
+        {
+            relativebase += program[firstparam];
+        }
+        bool JumpIfTrue(long firstparam, long secondparam)
+        {
+            if (program[firstparam] != 0)
+            {
+                driver = program[secondparam];
+                return true;
+            }
+            return false;
+        }
+        bool JumpIfFalse(long firstparam, long secondparam)
+        {
+            if (program[firstparam] == 0)
+            {
+                driver = program[secondparam];
+                return true;
+            }
+            return false;
+        }
+        void LessThan(long firstparam, long secondparam, long thirdparam)
+        {
+            if (program[firstparam] < program[secondparam])
+            {
+                program[thirdparam] = 1;
+            }
+            else
+                program[thirdparam] = 0;
+        }
+        void Equality(long firstparam, long secondparam, long thirdparam)
+        {
+            if (program[firstparam] == program[secondparam])
+            {
+                program[thirdparam] = 1;
+            }
+            else
+                program[thirdparam] = 0;
+        }
+        void Addition(long firstparam, long secondparam, long thirdparam)
+        {
+            program[thirdparam] = program[firstparam] + program[secondparam];
+        }
+        void Multiply(long firstparam, long secondparam, long thirdparam)
+        {
+            program[thirdparam] = program[firstparam] * program[secondparam];
+        }
+        void Input(long firstparam)
+        {
+            program[firstparam] = inputs.Dequeue();
+        }
+        void Output(long firstparam)
+        {
+            outputs.Add(program[firstparam]);
+        }
+        public void OverridePosition(int position, long value)
+        {
+            program[position] = value;
+        }
+        public long Position(int i)
+        {
+            return program[i];
+        }
+    }
+}
---
+++ D22/D22/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Data;
+using System.Reflection.Metadata.Ecma335;
+
+namespace D22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] commands = File.ReadAllLines(@"..\..\..\input.txt");
+            CardStack cs = new CardStack(10007);
+            foreach(string command in commands)
+                cs.Evaluate(command);
+            Console.WriteLine(cs.IndexOf(2019));
+
+            long factorynumber =    119315717514047;
+            long times =            101741582076661;
+        }
+    }
+    public class CardStack
+    {
+        int count;
+        int[] data;
+        public int IndexOf(int card) => data.ToList().IndexOf(card);
+        public int this[int card] => data[card];
+        public CardStack(int Cards) 
+        {
+            count = Cards;
+            data = new int[count];
+            for (int i = 0; i < count; i++)
+                data[i] = i;
+        }
+        public void Evaluate(string command)
+        {
+            string com = command.Split(' ')[0];
+            if (com == "cut")
+                Cut(int.Parse(command.Split(' ')[1]));
+            else if (int.TryParse(command.Split(' ').Last(), out int inc))
+                Deal(inc);
+            else
+                Deal();
+        }
+        public void Cut(int howmany)
+        {
+            int[] newdata = new int[count];
+            if (howmany < 0)
+            {
+                howmany *= -1;
+                for (int i = 0; i < howmany; i++)
+                    newdata[i] = data[count - howmany + i];
+                for (int i = howmany; i < count; i++)
+                    newdata[i] = data[i - howmany];
+            }
+            else
+            {
+                for (int i = 0; i < howmany; i++)
+                    newdata[count - howmany + i] = data[i];
+                for (int i = 0; i < count - howmany; i++)
+                    newdata[i] = data[howmany + i];
+            }
+            data = newdata;
+        }
+        public void Deal(int increment)
+        {
+            int[] newdata = new int[count];
+            for (int i = 0; i < count; i++)
+                newdata[(i * increment) % count] = data[i];
+            data = newdata;               
+        }
+        public void Deal()
+        {
+            int[] newdata = new int[count];
+            for (int i = 0; i < count; i++)
+                newdata[i] = data[count - 1 - i];
+            data = newdata;
+        }
+    }
+}
